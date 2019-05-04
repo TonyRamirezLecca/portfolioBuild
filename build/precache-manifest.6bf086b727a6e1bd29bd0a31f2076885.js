a39fb1a24f7456be5b3740cef96a75d2e50447f8
Tony Ramirez <tonyramirezlecca@gmail.com>
2019-05-04 00:17:18 -0600

    build for portfolio

---
+++ build/precache-manifest.6bf086b727a6e1bd29bd0a31f2076885.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "056fcc1bb4c5307e65b2978385de544b",
+    "url": "/static/media/lightsSM.056fcc1b.jpg"
+  },
+  {
+    "revision": "4d468b0ba440a0b96801",
+    "url": "/static/css/main.c3e3989e.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c2a8c14b6354463dbd2",
+    "url": "/static/js/2.b9113f5e.chunk.js"
+  },
+  {
+    "revision": "077d21248bbe9cc28eae83a971ea14d2",
+    "url": "/static/media/ResumePublic.077d2124.pdf"
+  },
+  {
+    "revision": "524a3322d1046046120af8424c3a8675",
+    "url": "/static/media/americanTaxi.524a3322.jpg"
+  },
+  {
+    "revision": "4d468b0ba440a0b96801",
+    "url": "/static/js/main.a70f404e.chunk.js"
+  },
+  {
+    "revision": "6506b41927a6c7268fd1eba10d523a5d",
+    "url": "/static/media/mapAPI.6506b419.jpg"
+  },
+  {
+    "revision": "eecb762f2d95ad9c7797b4b6d63e4b6e",
+    "url": "/static/media/me.eecb762f.jpg"
+  },
+  {
+    "revision": "f4e5eca2f9906166781036a81d7fd519",
+    "url": "/static/media/portfolio.f4e5eca2.jpg"
+  },
+  {
+    "revision": "b813152ef138c7c14220935b54336189",
+    "url": "/static/media/weather.b813152e.jpg"
+  },
+  {
+    "revision": "b1ce8ebe8fefe8854e274e4eb21dc2d8",
+    "url": "/static/media/xkcdComics.b1ce8ebe.jpg"
+  },
+  {
+    "revision": "bbb464036054df6a4f7002061713b5a5",
+    "url": "/index.html"
+  }
+];
